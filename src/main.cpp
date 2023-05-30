e046bc39e9cc7c3adfe8c5972f8a27362e72dfd0
dakshinatharindu <tharinduokd@gmail.com>
2023-05-31 00:06:40 +0530

    add lcd

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
 
 #include "_MQTT.h"
 #include "_WiFi.h"
 
+LiquidCrystal_I2C LCD = LiquidCrystal_I2C(0x27, 20, 4);
+
 void setup() {
     Serial.begin(115200);
+    delay(10);
+    LCD.init();
+    LCD.backlight();
+    LCD.setCursor(0, 0);
+    LCD.print("Hello World!");
     setupWiFi();
     setupMQTT();
 }
